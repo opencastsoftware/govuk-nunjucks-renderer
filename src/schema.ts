a8ffec355ca58d24437951430f1224cd0b3193ec
David Gregory <2992938+DavidGregory084@users.noreply.github.com>
2023-05-19 09:34:20 +0100

    Preprocess parameter schemas which contain dotted properties

--- src/schema.ts
+++ src/schema.ts
@@ -70,8 +70,59 @@ const createArraySchema = (params: PropertySchema[] | undefined) => {
   return params ? z.array(createObjectSchema(params)) : z.array(z.unknown());
 };
 
+const preProcessSchema = (params: PropertySchema[]): PropertySchema[] => {
+  if (params.length === 0) {
+    return params;
+  }
+
+  const empty: { [key: string]: PropertySchema[] } = {};
+
+  // Group by the dotted prefix
+  const groupedParams = params.reduce((grouped, param) => {
+    const path = param.name.split(".")[0] || param.name;
+    const group = grouped[path];
+    if (group) {
+      group.push(param);
+      return grouped;
+    } else {
+      return { ...grouped, [path]: [param] };
+    }
+  }, empty);
+
+  const preProcessed: PropertySchema[] = Object.entries(groupedParams).map(
+    ([path, innerParams]) => {
+      innerParams.forEach((innerParam) => {
+        const paramSegments = innerParam.name.split(".");
+        if (paramSegments.length > 1) {
+          // None of the current params in govuk-frontend have more than two segments
+          innerParam.name = paramSegments[1] || innerParam.name;
+        }
+        if (innerParam.params) {
+          innerParam.params = preProcessSchema(innerParam.params);
+        }
+      });
+
+      if (innerParams.length === 1 && innerParams[0]) {
+      // This is a singleton group
+        return innerParams[0];
+      } else {
+        // Create a synthetic object to hold the grouped parameters of this prefix
+        return {
+          name: path,
+          type: "object",
+          required: true,
+          // Some parameters collide with this synthetic object
+          params: innerParams.filter((innerParam) => innerParam.name !== path),
+        };
+      }
+    }
+  );
+
+  return preProcessed;
+};
+
 export const createZodSchema = (data: ComponentSchema) => {
   return z.object({
-    params: createParamsSchema(data.params),
+    params: createParamsSchema(preProcessSchema(data.params)),
   });
 };
